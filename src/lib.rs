0c982106c53abd97a79fa8263be42b12641438d8
Srylax <71783705+Srylax@users.noreply.github.com>
2025-04-05 11:05:51 +0200

    feat: add legacy data

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-use bitflags::bitflags;
 use chrono::{DateTime, Utc};
-use num_enum::{IntoPrimitive, TryFromPrimitive};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -13,64 +11,11 @@ pub struct BLEData {
     pub addr: [u8; 6],
 }
 
-/// A Bluetooth UUID.
-#[derive(Copy, Clone, Debug)]
-pub enum BleUuid {
-    /// A 16-bit UUID.
-    Uuid16(u16),
-    /// A 32-bit UUID.
-    Uuid32(u32),
-    /// A 128-bit UUID.
-    Uuid128([u8; 16]),
-}
-
-bitflags! {
-  #[repr(transparent)]
-  #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-  pub struct AdvFlag: u8 {
-    /// LE Limited Discoverable Mode
-    const DiscLimited = 1 as _;
-    /// LE General Discoverable Mode
-    const DiscGeneral = 2 as _;
-    /// BR/EDR Not Supported
-    const BrEdrUnsupported = 4 as _;
-    /// Simultaneous LE and BR/EDR to Same Device Capable (Controller)
-    const SimultaneousController = 0b01000;
-    /// Simultaneous LE and BR/EDR to Same Device Capable (Host)
-    const SimultaneousHost       = 0b10000;
-  }
-}
-
-#[repr(u8)]
-#[derive(Copy, Clone, PartialEq, Debug, TryFromPrimitive, IntoPrimitive)]
-pub enum PrimPhy {
-    /// 1Mbps phy
-    Phy1M = 1 as _,
-    /// Coded phy
-    Coded = 3 as _,
-}
-
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub enum AdvType {
-    /// indirect advertising
-    Ind,
-    /// direct advertising
-    DirectInd,
-    /// indirect scan response
-    ScanInd,
-    /// indirect advertising - not connectable
-    NonconnInd,
-    ScanResponse,
-    // #[cfg(esp_idf_bt_nimble_ext_adv)]
-    // Extended(u8),
-}
-
-/// Bluetooth Device address type
-#[derive(PartialEq, Eq, TryFromPrimitive, Debug, Clone)]
-#[repr(u8)]
-pub enum BLEAddressType {
-    Public = 0 as _,
-    Random = 1 as _,
-    PublicID = 2 as _,
-    RandomID = 3 as _,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct LegacyBLEData {
+    pub device_id: String,
+    pub time_stamp: DateTime<Utc>,
+    pub company_identifier: Option<u16>,
+    pub rssi: i8,
+    pub addr: [u8; 6],
 }
